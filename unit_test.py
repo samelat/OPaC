28b5eb0dfda913794a081946635608d8c7fa1d9c
Gaston Traberg <gtraberg@cert.unlp.edu.ar>
2015-08-10 14:34:53 -0300

    unit test fix

--- unit_test.py
+++ unit_test.py
@@ -11,26 +11,26 @@ fd.close()
 paths = [urlparse.urlsplit(uri).path.strip() for uri in uris]
 
 opac = OPaC()
-for path in paths[:1000]:
+for path in paths[:200]:
     opac.add_path(urllib.unquote(path))
 
 opac.tree.print_tree()
 
-print 'WEIGHT: {0} - SIZE: {1}'.format(opac.tree.weight, opac.tree.size())
+print 'WEIGHT: {0}'.format(opac.tree.weight)
 
 first_len = len(opac.paths)
 opac.clean()
 last_len = len(opac.paths)
 
 print '{0}/{1}'.format(last_len, first_len)
-print 'WEIGHT: {0} - SIZE: {1}'.format(opac.tree.weight, opac.tree.size())
+print 'WEIGHT: {0}'.format(opac.tree.weight)
 
 print '*'*50
 
-for path in paths[1000:]:
+for path in paths[200:]:
     opac.add_path(path)
 
-print 'WEIGHT: {0} - SIZE: {1}'.format(opac.tree.weight, opac.tree.size())
+print 'WEIGHT: {0}'.format(opac.tree.weight)
 
 first_len = len(opac.paths)
 opac.clean()
@@ -38,6 +38,6 @@ last_len = len(opac.paths)
 
 print '{0}/{1}'.format(last_len, first_len)
 
-print 'WEIGHT: {0} - SIZE: {1}'.format(opac.tree.weight, opac.tree.size())
+print 'WEIGHT: {0}'.format(opac.tree.weight)
 
 opac.tree.print_tree()
