a627a8830bbf52565cb83598de41d887eb0d832e
Gaston Traberg <samelat@gmail.com>
2015-08-18 01:16:01 -0300

    I'm refactoring again; It's not working

--- opac_regex.py
+++ opac_regex.py
@@ -1,119 +1,79 @@
 import re
 
+''' ##############################################################
+
+    ##############################################################
+'''
 class OPaCRegex:
-    def __init__(self, entries):
-        self.entries = {}
-        for entry in entries:
-            self.entries[entry] = {}
 
-    def fragment(self, groups):
-        result = []
-        sizes = []
+    def __init__(self):
+        self.samples = []
 
-        last_group = groups.pop(0)
-        while groups:
-            group = groups.pop(0)
+    def digest(self, samples):
 
-            intersection_size = 0
-            for size in range(1, min(len(last_group), len(group)) + 1):
-                if last_group[-1*size:] == group[:size]:
-                    intersection_size = size
-                    break
+        templates = {}
+        for sample in samples:
+            print(sample)
+            sample = Sample(sample)
+            template = tuple(sample.template)
 
-            if intersection_size:
-                fragments = [last_group[:-1*intersection_size],
-                             last_group[ -1*intersection_size:],
-                             group[:intersection_size],
-                             group[intersection_size:]]
-                fragments = [group for group in fragments if group]
+            if template not in templates:
+                templates[template] = []
+            templates[template].append(sample)
 
-                last_group = fragments.pop()
-                result.extend(fragments)
+        print(templates)
 
-            else:
-                result.append(last_group)
-                last_group = group
+        # Select Best Template and Its samples
+        sorted_templates = [(len(samples), template) for template, samples in templates.items()]
+        sorted_templates.sort(reverse=True)
+        best_template = sorted_templates[0][1]
+        self.samples = templates[best_template]
 
-        result.append(last_group)
+        template = Template(best_template, self.samples)
 
-        return result
+        template.sharpen()
+        #return template.compile()
 
 
-    def compress(self, groups):
-        result = []
-        sizes = []
+''' ##############################################################
 
-        group_size = 1
-        last_group = groups.pop(0)
-        while groups:
-            group = groups.pop(0)
-            if last_group == group:
-                group_size += 1
-            else:
-                result.append(last_group)
-                sizes.append(group_size)
-                last_group = group
-                group_size = 1
+    ##############################################################
+'''
+class Template:
+    def __init__(self, template, samples):
+        self.samples = samples
+        self.template = template
 
-        sizes.append(group_size)
-        result.append(last_group)
+    def sharpen(self):
 
-        return result, sizes
+        #heights = [sample.heights for sample in self.samples]
+        heights_union = [set([height]) for height in self.samples[0].heights]
+
+        for height_index in range(0, len(heights_union)):
+            heights_union[height_index].update([sample.heights[height_index] for sample in self.samples[1:]])
 
 
-    def group_tokens(self, sizes, tokens):
-        result = []
-        last_index = 0
-        for height, weight in sizes:
-            row = []
-            for index in range(last_index, last_index + height*weight, weight):
-                entry = ''.join(tokens[index:index+weight])
-                row.append(entry)
 
-                last_index = index + weight
+        #grouped_tokens_union = [set(token) for token in self.samples[0].group_tokens()]
+        samples_grouped_tokens = [sample.group_tokens() for sample in self.samples]
 
-            result.append(row)
+        for grouped_tokens in samples_group_tokens:
+            for tokens_group in grouped_tokens:
 
-        return result
+        print(all_grouped_tokens[0])
 
+        grouped_tokens_union = []
+        for group_index in range(0, len(self.template)):
+            group_union = []
+            for token_index in range(0, len(self.template[group_index])):
+                for grouped_tokens in all_grouped_tokens
+                tokens_union = set([grouped_tokens[group_index][token_index] ])
+                group_union.append(tokens_union)
+            grouped_tokens_union.append(group_union)
 
-    def template(self, tokens):
+        print(grouped_tokens_union)
 
-        group = []
-        template = []
-        for token in tokens:
-            if re.match('\d', token[0]):
-                regex_token = '\d'
-
-            elif re.match('[^\W_]', token[0]):
-                regex_token = '[^\W_]'
-
-            else:
-                regex_token = '\\' + token[0]
-
-            if regex_token in group:
-                template.append(tuple(group))
-                group = []
-            
-            group.append(regex_token)
-
-        template.append(tuple(group))
-        return template
-
-
-    def sharpen(self, template):
-        match_entries = [entry for entry in self.entries.values() if entry['template'] == template]
-
-        groups_heights = [entry['heights'] for entry in match_entries]
-        groups_heights_union = [set([height]) for height in groups_heights[0]]
-        for height_index in range(0, len(groups_heights_union)):
-            groups_heights_union[height_index].update([heights[height_index] for heights in groups_heights[1:]])
-
-        grouped_tokens = [entry['grouped_tokens'] for entry in match_entries]
-        grouped_tokens_union = [set(tokens_group) for tokens_group in grouped_tokens[0]]
-        for tokens_group in grouped_tokens[1:]:
-            for group_index in range(0, len(grouped_tokens_union)):
-                grouped_tokens_union[group_index] = grouped_tokens_union[group_index].intersection(tokens_group[group_index])
+        return
 
         sharped_heights = []
         sharped_template = []
@@ -137,6 +97,10 @@ class OPaCRegex:
 
 
     def compile(self, template, heights):
+        print('[compile] heights: {0}'.format(heights))
+        print('[compile] template: {0}'.format(template))
+        for entry in self.entries.values():
+            print('[compile] entry: {0}'.format(entry['grouped_tokens']))
 
         regex = ''
         for group_index in range(0, len(template)):
@@ -171,36 +135,108 @@ class OPaCRegex:
         return regex
 
 
-    def digest(self):
+''' ##############################################################
 
-        templates = {}
-        for entry in self.entries:
-            tokens = re.findall('(\d+|[^\W_]+|[\W_])', entry)
+    ##############################################################
+'''
+class Sample:
 
-            template = self.template(tokens)
-            template = self.fragment(template)
-            template, heights = self.compress(template)
+    def __init__(self, sample):
+        self.tokens = re.findall('([^\W_]+|[\W_])', sample)
+        template = self.digest_tokens()
+        template = self.fragment(template)
+        self.template, self.heights = self.compress(template)
 
-            # El template tendria que ser como el Identificador del proceso
-            # que se esta haciendo, entonces en group_tokens, solo se deberia
-            # especificar los tokens y el template (las unicas 2 cosas que se
-            # necesitan :) ).
-            weights = [len(group) for group in template]
-            grouped_tokens = self.group_tokens(list(zip(heights, weights)), tokens)
-            self.entries[entry]['grouped_tokens'] = grouped_tokens
-            self.entries[entry]['heights'] = heights
+    def __hash__(self):
+        return hash(tuple(self.template))
 
-            template = tuple(template)
-            self.entries[entry]['template'] = template
+    def group_tokens(self):
+        weights = [len(group) for group in self.template]
 
-            if template not in templates:
-                templates[template] = 0
-            templates[template] += 1
+        result = []
+        last_index = 0
+        for height, weight in zip(self.heights, weights):
+            group = []
+            for index in range(last_index, last_index + height*weight, weight):
+                row.append(self.tokens[index:index+weight])
 
-        sorted_templates = [(value, template) for template,value in templates.items()]
-        sorted_templates.sort(reverse=True)
-        best_template = sorted_templates[0][1]
+                last_index = index + weight
 
-        sharped_template, sharped_heights = self.sharpen(best_template)
+            result.append(group)
+        return result
 
-        return self.compile(sharped_template, sharped_heights)
+    def digest_tokens(self):
+        regexes = []
+        group = []
+        for token in self.tokens:
+            if re.match('^\d+$', token):
+                regex_token = '\d'
+
+            elif re.match('[^\W_]+', token[0]):
+                regex_token = '[^\W_]'
+
+            else:
+                regex_token = '\\' + token[0]
+
+            if regex_token in group:
+                regexes.append(tuple(group))
+                group = []
+            
+            group.append(regex_token)
+
+        regexes.append(tuple(group))
+        return regexes
+
+
+    def fragment(self, template):
+        fragmented_template = []
+        sizes = []
+
+        last_group = template.pop(0)
+        while template:
+            group = template.pop(0)
+
+            intersection_size = 0
+            for size in range(1, min(len(last_group), len(group)) + 1):
+                if last_group[-1*size:] == group[:size]:
+                    intersection_size = size
+                    break
+
+            if intersection_size:
+                fragments = [last_group[:-1*intersection_size],
+                             last_group[ -1*intersection_size:],
+                             group[:intersection_size],
+                             group[intersection_size:]]
+                fragments = [group for group in fragments if group]
+
+                last_group = fragments.pop()
+                fragmented_template.extend(fragments)
+
+            else:
+                fragmented_template.append(last_group)
+                last_group = group
+
+        fragmented_template.append(last_group)
+        return fragmented_template
+
+
+    def compress(self, template):
+        result = []
+        sizes = []
+
+        group_size = 1
+        last_group = template.pop(0)
+        while template:
+            group = template.pop(0)
+            if last_group == group:
+                group_size += 1
+            else:
+                result.append(last_group)
+                sizes.append(group_size)
+                last_group = group
+                group_size = 1
+
+        sizes.append(group_size)
+        result.append(last_group)
+        return result, sizes
+
